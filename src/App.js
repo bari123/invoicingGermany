d2380455cff1e32b4e24151cedde40cd6ce41ce5
Bari Ademi <baroademi@gmail.com>
2024-09-16 20:03:39 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const InputField = ({ label, value, onChange }) => (
+    <label>
+        {label}
+        <input value={value} onChange={onChange} />
+    </label>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [clientName, setClientName] = useState('');
+    const [clientAddress, setClientAddress] = useState('');
+    const [rech, setRech] = useState('');
+    const [leist, setLeist] = useState('');
+    const [rechnungNr, setRechnungNr] = useState('');
+    const [kdNr, setKdNr] = useState('');
+    const [aufftr, setAufftr] = useState('');
+    const [deadline, setDeadline] = useState('');
+    const [percentage, setPercentage] = useState('');
+
+    const [inputData, setInputData] = useState({
+        column1: '',
+        column2: '',
+        column3: '',
+        column4: '',
+        column5: ''
+    });
+    const [tableData, setTableData] = useState([]);
+
+    const handleAddRow = () => {
+        setTableData([...tableData, inputData]);
+        setInputData({ column1: '', column2: '', column3: '', column4: '', column5: '' });
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setInputData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    return (
+        <div className="App">
+            <div style={{display:'flex',width:'100%',justifyContent:'space-around'}}>
+
+            <fieldset>
+                <legend>Client Information</legend>
+                <InputField label="Name" value={clientName} onChange={e => setClientName(e.target.value)} />
+                <InputField label="Address" value={clientAddress} onChange={e => setClientAddress(e.target.value)} />
+            </fieldset>
+
+            <fieldset>
+                <legend>Invoice Information</legend>
+                <InputField label="Rechn.-Datum" value={rech} onChange={e => setRech(e.target.value)} />
+                <InputField label="Leist.-Datum" value={leist} onChange={e => setLeist(e.target.value)} />
+                <InputField label="Rechnung-Nr." value={rechnungNr} onChange={e => setRechnungNr(e.target.value)} />
+                <InputField label="Kd-Nummer" value={kdNr} onChange={e => setKdNr(e.target.value)} />
+                <InputField label="Aufftr.-Nummer" value={aufftr} onChange={e => setAufftr(e.target.value)} />
+            </fieldset>
+            </div>
+
+            <fieldset>
+                <legend>Other Information</legend>
+                <InputField label="Frist" value={deadline} onChange={e => setDeadline(e.target.value)} />
+                <InputField label="Prozentsatz" value={percentage} onChange={e => setPercentage(e.target.value)} />
+            </fieldset>
+
+            <div className="table-container">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Pos.nr</th>
+                        <th>Bezeichnung</th>
+                        <th>Menge</th>
+                        <th>Einheit</th>
+                        <th>E-Preis</th>
+                        <th>G-Preis</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tableData.map((row, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{row.column1}</td>
+                            <td>{row.column2}</td>
+                            <td>{row.column3}</td>
+                            <td>{row.column4}</td>
+                            <td>{row.column5}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+
+                <div>
+                    <input type="text" name="column1" placeholder="Bezeichnung" value={inputData.column1} onChange={handleInputChange} />
+                    <input type="text" name="column2" placeholder="Menge" value={inputData.column2} onChange={handleInputChange} />
+                    <input type="text" name="column3" placeholder="Einheit" value={inputData.column3} onChange={handleInputChange} />
+                    <input type="text" name="column4" placeholder="E-Preis" value={inputData.column4} onChange={handleInputChange} />
+                    <input type="text" name="column5" placeholder="G-Preis" value={inputData.column5} onChange={handleInputChange} />
+                    <button onClick={handleAddRow}>+ Add Row</button>
+                </div>
+            </div>
+            <fieldset style={{marginTop:50,display:'flex',width:'100%',justifyContent:'space-around'}}>
+              <legend>Actions</legend>
+            <button>Print</button>
+            <button>Print    Preview</button>
+            <button>Download</button>
+            </fieldset>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/printablefile.js
@@ -0,0 +1,133 @@
+export function PrintableFile ({client,invoiceInfo,articles,deadline,percentage}){
+    const nettoSum=articles.reduce((sum,item)=>sum+item.gPreis,0)
+
+    return (
+        <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
+            <p style={{textAlign: 'center', fontWeight: 'bold', borderBottom: '2px solid black'}}>METI-Construction
+                UG</p>
+            <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', marginBottom: 50}}>
+                <div style={{width: '21%', fontSize: '10px', lineHeight: 1.5}}>
+                    <div style={{textDecoration: 'underline', fontSize: '5px'}}>METI-Construction UG - Uhlenhorst
+                        149A - 21435 Stelle
+                    </div>
+                    <div>{client.name}</div>
+                    <div>{client.addresS}</div>
+                </div>
+                <div style={{width: '20%', fontSize: '10px', textAlign: 'end', lineHeight: 1.5}}>
+                    <div>METI-CONSTRUCTION</div>
+                    <div>Uhlenhorst 149A</div>
+                    <div>21435 Stelle</div>
+                    <div>Tel.:+49 1522 3681507</div>
+                    <div>Steuernummer: 50/202/04222</div>
+                    <div>Finanzaamt Winsen/Luhe</div>
+                </div>
+            </div>
+            <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', marginBottom: 50}}>
+                <div style={{fontSize: '12px', display: 'flex', flexDirection: 'column'}}>
+                    <div>Diverse Bauhaben</div>
+                    <p style={{fontWeight: 'bolder', textAlign: 'center'}}>Rechnung</p>
+                    <div>Sehr geehrte Damen und Herren</div>
+                    <div>hiermit stellen wir Ihnen folgende Leistungen in rechnung</div>
+                </div>
+                <div style={{fontSize: '12px', width: '30%', margin: 0}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>Rechn.-Datum</div>
+                        <div>{invoiceInfo.rech}</div>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>Leist.-Datum</div>
+                        <div>{invoiceInfo.leist}</div>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>Rechnung-Nr.</div>
+                        <div>{invoiceInfo.rechnungNr}</div>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>Kd-Nummer</div>
+                        <div>{invoiceInfo.kdNr}</div>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <div>Aufftr.-Nummer</div>
+                        <div>{invoiceInfo.aufftr}</div>
+                    </div>
+                </div>
+            </div>
+            <div style={{width: '100%', marginBottom: 50, display: 'flex', flexDirection: 'column'}}>
+                <table width={'100%'}>
+                    <tbody>
+                    <tr style={{border: '1px solid black'}}>
+                        <th>Pos-Nr</th>
+                        <th style={{textAlign:'left'}}>Bezeichnung</th>
+                        <th>Menge</th>
+                        <th>Einheit</th>
+                        <th>E-Preis</th>
+                        <th>G-Preis</th>
+                    </tr>
+                    {articles && articles.map((x, index) => {
+                        return (
+                            <tr  style={{textAlign:'center'}}>
+                                <td >{index}.0</td>
+                                <td style={{textAlign:'left',textWrap:'wrap',maxWidth:'300px'}} >{x.Bezeichnung}</td>
+                                <td>{x.Menge}</td>
+                                <td>{x.Einheit}</td>
+                                <td>{x.ePreis},00</td>
+                                <td >{x.gPreis},00</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+                <div style={{paddingLeft: '60%', marginTop: 10,display:'grid',flexDirection:'column'}}>
+                    <div>Positionsfortsetzung nächste seite</div>
+                    <div style={{width:'150px',background: 'lightgreen',textAlign:'center',padding:5 ,borderBottom:'2px solid black',color: 'white',justifySelf:"end"}}>
+                        Übertrag: <span>€ {nettoSum},00</span>
+                    </div>
+                </div>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'end',
+                paddingLeft: "50%",
+                gap: 10,
+                marginBottom: 50
+            }}>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    justifySelf: 'end',
+                    borderBottom: '1px solid black'
+                }}>
+                    <div>Nettosumme</div>
+                    <div>€ {nettoSum},00</div>
+                </div>
+                {
+                 percentage &&
+                <div style={{display: 'flex', justifyContent: 'space-around', alignContent: 'end'}}>
+                    <div>
+                        MwSt. 19.00%
+                    </div>
+                    <div>
+                        von {nettoSum}
+                    </div>
+                    <div>
+                        € {percentage}
+                    </div>
+                </div>
+                }
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    justifySelf: 'end',
+                    borderBottom: '1px solid black'
+                }}>
+                    <div>Bruttosumme</div>
+                    <div>€ { nettoSum + percentage}</div>
+                </div>
+            </div>
+            <div>
+                Zahlbar bis zum {deadline} ohne Abzug.
+            </div>
+        </div>
+    )
+}
